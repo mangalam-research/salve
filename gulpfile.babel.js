c19ec989d3837223f5746f1b5cc87579a363badf
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-05-22 18:53:17 -0700

    Add destination for arg, update README.rst.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -61,17 +61,18 @@ parser.addArgument(['--jsdoc'], {
 });
 
 parser.addArgument(["--jsdoc-private"], {
-    help: "Jsdoc will document private functions.",
+    help: "jsdoc will document private functions.",
     type: Boolean,
     action: 'storeTrue',
     defaultValue: local_config.jsdoc_private
 });
 
 parser.addArgument(["--no-jsdoc-private"], {
-    help: "Jsdoc will not document private functions.",
+    help: "jsdoc will not document private functions.",
     type: Boolean,
     action: 'storeFalse',
-    defaultValue: local_config.jsdoc_private
+    dest: 'jsdoc_private',
+    defaultValue: local_config.no_jsdoc_private
 });
 
 parser.addArgument(["--jsdoc-required-version"], {
