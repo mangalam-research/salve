83b909dab7a66a51c8e7fe6391fd809d28a4832e
Louis-Dominique Dubeau <ldd@lddubeau.com>
2017-05-24 13:07:21 -0400

    Version bump.

--- lib/salve/validate.ts
+++ lib/salve/validate.ts
@@ -6,7 +6,7 @@
  * @copyright Mangalam Research Center for Buddhist Languages
  */
 
-export const version: string = "4.1.0";
+export const version: string = "4.1.1";
 
 export { eventsToTreeString, Event, EventSet, Grammar, GrammarWalker,
          BasePattern, RefError, Walker, __test, FireEventResult,
