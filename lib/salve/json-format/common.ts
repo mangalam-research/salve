3ee28ab9cb50a2897a53af436a1c73928fab9f7e
Louis-Dominique Dubeau <ldd@lddubeau.com>
2018-04-25 15:53:07 -0400

    Remove nameToConstructor.
    It had ceased to be used a while back.

--- lib/salve/json-format/common.ts
+++ lib/salve/json-format/common.ts
@@ -74,58 +74,6 @@ nameToCode.NameChoice = 19;
 nameToCode.NsName = 20;
 nameToCode.AnyName = 21;
 
-//
-// MODIFICATIONS TO THIS MAP MUST BE REFLECTED IN ALL OTHER TABLES IN THIS
-// MODULE.
-//
-export const nameToConstructor: Record<string, Ctors> = {
-  // Array = 0 is hard-coded elsewhere in the conversion code so don't change
-  // it.
-  0: Array,
-  Empty,
-  1: Empty,
-  Data,
-  2: Data,
-  List,
-  3: List,
-  Param,
-  4: Param,
-  Value,
-  5: Value,
-  NotAllowed,
-  6: NotAllowed,
-  Text,
-  7: Text,
-  Ref,
-  8: Ref,
-  OneOrMore,
-  9: OneOrMore,
-  Choice,
-  10: Choice,
-  Group,
-  11: Group,
-  Attribute,
-  12: Attribute,
-  Element,
-  13: Element,
-  Define,
-  14: Define,
-  Grammar,
-  15: Grammar,
-  EName,
-  16: EName,
-  Interleave,
-  17: Interleave,
-  Name,
-  18: Name,
-  NameChoice,
-  19: NameChoice,
-  NsName,
-  20: NsName,
-  AnyName,
-  21: AnyName,
-};
-
 // This is a bit field
 export const OPTION_NO_PATHS = 1;
 // var OPTION_WHATEVER = 2;
